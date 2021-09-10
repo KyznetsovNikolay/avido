c7a0e286b691bef4d3a7e1ddf91df426a33bdf99
Nikolay Kyznetsov <kyznetsovnic@gmail.com>
2021-09-10 09:55:01 +0300

    Adde filter for user list

--- app/Http/Controllers/Admin/Users/Crud/IndexAction.php
+++ app/Http/Controllers/Admin/Users/Crud/IndexAction.php
@@ -4,13 +4,42 @@ namespace App\Http\Controllers\Admin\Users\Crud;
 
 use App\Http\Controllers\Controller;
 use App\Models\User\User;
+use Illuminate\Http\Request;
 
 class IndexAction extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        $users = User::orderBy('id', 'desc')->paginate(10);
+        $users = $this->withFilter($request)->paginate(10);
+
         $roles = User::getRoleLabels();
         return view('admin.users.index', compact('users', 'roles'));
+    }
+
+    public function withFilter(Request $request)
+    {
+        $query = User::orderByDesc('id');
+
+        if (!empty($value = $request->get('id'))) {
+            $query->where('id', $value);
+        }
+
+        if (!empty($value = $request->get('name'))) {
+            $query->where('name', 'like', '%' . $value . '%');
+        }
+
+        if (!empty($value = $request->get('email'))) {
+            $query->where('email', 'like', '%' . $value . '%');
+        }
+
+        if (!empty($value = $request->get('status'))) {
+            $query->where('status', $value);
+        }
+
+        if (!empty($value = $request->get('role'))) {
+            $query->where('role', $value);
+        }
+
+        return $query;
     }
 }
